5fffb8fe089392af2594d362999467d862e523cd
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:29:42 +0100

    Update cercadip.php

--- cercadip.php
+++ cercadip.php
@@ -5,12 +5,13 @@ $password=""; // Mysql password
 $db_name="my_setlist"; //Nome del Database
 $tbl_name="members"; // Nome della Tabella
 // Procedimento per connettersi al Database
-mysqli_connect("$host", "$username", getPassword());
-mysqli_select_db("$db_name"); 
+$connection = new mysqli($host, $username, $password, $db);
+$db =mysqli_select_db("$db_name"); 
 // Nome utente e password inviate attraverso il form
 $myusername=$_POST['myusername'];
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and tipo='Dipendente'";
-$result=mysql_query($sql);
+$query1 = sprintf("SELECT * FROM $tbl_name WHERE username='$myusername' and tipo='Dipendente'" ,mysqli_real_escape_string($connection,$username);
+$result =  mysqli_query($connection, $query1);
+
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
