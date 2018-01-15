de3baa6f57a64d22e06e016669a99544097d32c7
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:39:37 +0100

    Update creacliente.php

--- creacliente.php
+++ creacliente.php
@@ -5,8 +5,8 @@ $username="setlist"; // Mysql username
 $password=""; // Mysql password
 $db_name="my_setlist"; //Nome del Database
 $tbl_name="members"; // Nome della Tabella
-$db = mysql_connect($localhost,$username,$password);
-mysql_select_db($db_name,$db);
+$connection = new mysqli($host, $username, $password, $db);
+$db =mysqli_select_db("$db_name"); 
 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -17,11 +17,11 @@ $email = $_POST['email'];
 $NumeroTel = $_POST['NumeroTel'];
 $Codice = $_POST['Codice'];
 $codloginext = $_POST['codloginext'];
-
-mysql_query("INSERT INTO ".$tbl_name." (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Cliente','$codloginext')",$db) || die(mysql_error());
+$query1 = sprintf("INSERT INTO ".$tbl_name." (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Cliente','$codloginext')");
+$ses_sql =  mysqli_query($connection, $query1);
 
 header("location:clicreato.php");
 
 /* quindi i dati sono stati salvati nel database e l'utente è registrato */
 mysql_close($db);
-?> 
+?> 
