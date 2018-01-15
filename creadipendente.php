b3367583cd01f55add19fe5a6ff76e617577f3eb
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:57:54 +0100

    Update creadipendente.php

--- creadipendente.php
+++ creadipendente.php
@@ -18,10 +18,21 @@ $NumeroTel = $_POST['NumeroTel'];
 $Codice = $_POST['Codice'];
 $codloginext = $_POST['codloginext'];
 
-mysql_query("INSERT INTO ".$tbl_name." (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Dipendente','$codloginext')",$db) || die(mysql_error());
-
+$query1 = sprintf("INSERT INTO 'members' (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Dipendente','$codloginext')",
+                  	mysqli_real_escape_string($connessione,$username),
+					mysqli_real_escape_string($connessione,$password),
+					mysqli_real_escape_string($connessione,$Nome),
+					mysqli_real_escape_string($connessione,$Cognome),
+                  mysqli_real_escape_string($connessione,$Indirizzo),
+                  mysqli_real_escape_string($connessione,$email),
+                  mysqli_real_escape_string($connessione,$NumeroTel),
+                  mysqli_real_escape_string($connessione,$Codice),
+                  mysqli_real_escape_string($connessione,$Tipo),
+                  mysqli_real_escape_string($connessione,$codloginext),
+			);
+$ses_sql =  mysqli_query($connection, $query1);
 header("location:dipcreato.php");
 
 /* quindi i dati sono stati salvati nel database e l'utente è registrato */
 mysql_close($db);
-?> 
+?> 
