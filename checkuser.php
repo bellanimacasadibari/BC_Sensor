0c7460192ef88e7ea98923955caecd1430b3dcb5
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:36:40 +0100

    Update checkuser.php

--- checkuser.php
+++ checkuser.php
@@ -5,12 +5,13 @@ $password=""; // Mysql password
 $db_name="my_setlist"; //Nome del Database
 $tbl_name="members"; // Nome della Tabella
 // Procedimento per connettersi al Database
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB"); 
+$connection = new mysqli($host, $username, $password, $db);
+$db =mysqli_select_db("$db_name"); 
 // Nome utente e password inviate attraverso il form
 $mycodloginext=$_POST['mycodloginext'];
-$sql="SELECT * FROM $tbl_name WHERE codloginext='$mycodloginext'";
-$result=mysql_query($sql);
+$query1 = sprintf("SELECT * FROM $tbl_name WHERE codloginext ='$mycodloginext'" ,mysqli_real_escape_string($connection,$codloginext);
+$result =  mysqli_query($connection, $query1);
+
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
@@ -23,4 +24,4 @@ if($count==1){
 else {
 echo "Attenzione utente non esistente!";
 }
-?> 
+?> 
