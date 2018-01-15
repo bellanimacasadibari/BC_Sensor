d387d251087229fb2f61cf757549042c5451057d
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:56:29 +0100

    Update creacliente.php

--- creacliente.php
+++ creacliente.php
@@ -17,7 +17,18 @@ $email = $_POST['email'];
 $NumeroTel = $_POST['NumeroTel'];
 $Codice = $_POST['Codice'];
 $codloginext = $_POST['codloginext'];
-$query1 = sprintf("INSERT INTO ".$tbl_name." (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Cliente','$codloginext')");
+$query1 = sprintf("INSERT INTO 'members' (username,password,Nome,Cognome,Indirizzo,email,NumeroTel,Codice,Tipo,codloginext) VALUES ('$username','$password','$Nome','$Cognome','$Indirizzo','$email','$NumeroTel','$Codice','Cliente','$codloginext')",
+                  	mysqli_real_escape_string($connessione,$username),
+					mysqli_real_escape_string($connessione,$password),
+					mysqli_real_escape_string($connessione,$Nome),
+					mysqli_real_escape_string($connessione,$Cognome),
+                  mysqli_real_escape_string($connessione,$Indirizzo),
+                  mysqli_real_escape_string($connessione,$email),
+                  mysqli_real_escape_string($connessione,$NumeroTel),
+                  mysqli_real_escape_string($connessione,$Codice),
+                  mysqli_real_escape_string($connessione,$Tipo),
+                  mysqli_real_escape_string($connessione,$codloginext),
+			);
 $ses_sql =  mysqli_query($connection, $query1);
 
 header("location:clicreato.php");
