1de95036ca973e1962b0f06590b4911950582359
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 10:49:05 +0100

    Update cercadipdelete.php

--- cercadipdelete.php
+++ cercadipdelete.php
@@ -5,12 +5,13 @@ $password=""; // Mysql password
 $db_name="my_setlist"; //Nome del Database
 $tbl_name="members"; // Nome della Tabella
 // Procedimento per connettersi al Database
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB"); 
+$connection = new mysqli($host, $username, getPassword(), $db);
+$db =mysqli_select_db("$db_name"); 
 // Nome utente e password inviate attraverso il form
 $myusername=$_POST['myusername'];
+$query1 = sprintf("SELECT * FROM $tbl_name WHERE username ='$user_check' and tipo='Dipendente'",mysqli_real_escape_string($connection,$codsensore);
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and tipo='Dipendente'";
-$result=mysql_query($sql);
+$result= mysqli_query($connection, $query1);
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
@@ -29,4 +30,4 @@ if(isset($_POST['myusername'])){
 else {
 echo "Attenzione username errato";
 }}
-?> 
+?> 
