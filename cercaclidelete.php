ee9cbd141fc9233bd60f63e2ec3bd637ae803fae
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-15 17:31:40 +0100

    Update cercaclidelete.php

--- cercaclidelete.php
+++ cercaclidelete.php
@@ -10,8 +10,8 @@ $db =mysql_select_db("$db_name");
 // Nome utente e password inviate attraverso il form
 $myusername=$_POST['myusername'];
 $query1 = sprintf("SELECT * FROM $tbl_name WHERE username='$myusername' and tipo='Cliente'" ,mysqli_real_escape_string($connection,$username);
-$sql= mysqli_query($connection, $query1);
-$result=mysql_query($sql);
+$result= mysqli_query($connection, $query1);
+
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
